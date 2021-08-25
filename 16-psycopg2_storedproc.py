e684037a56359284c5d84ca0574f6d51fa36130c
Antonio Vilchez <antoniovmonge@gmail.com>
2021-08-25 21:16:28 +0200

    more

---
+++ 16-psycopg2_storedproc.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+if __name__ == "__main__":
+	conn = psycopg2.connect(database="red30",
+		user="antonio",
+		password="password",
+		host="localhost",
+		port="5432")
+
+	conn.autocommit = True
+	
+	cur = conn.cursor()
+
+	cur.execute('''CALL return_nondiscounted_item (%s, %s)''', (1105910, 1))
+	
+	# How to Call Postgres Function: 
+	# cur.callproc('return_nondiscounted_item', (1105910, 1))
+
+	conn.close()
+    
